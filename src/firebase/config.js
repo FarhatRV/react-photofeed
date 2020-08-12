299b7eb94528bbeb64d0ec0bd8be1a6678737b67
Farhat Abbas RV <abbasfarhat47@gmail.com>
2020-08-12 21:12:13 +0530

    remove firebase details

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDLmnoBvnjOQ-AaJgDw1DnVBiTETbcp8LI",
-  authDomain: "the-net-ninja-sandbox.firebaseapp.com",
-  databaseURL: "https://the-net-ninja-sandbox.firebaseio.com",
-  projectId: "the-net-ninja-sandbox",
-  storageBucket: "the-net-ninja-sandbox.appspot.com",
-  messagingSenderId: "485942827092",
-  appId: "1:485942827092:web:1811d9d8f1f5fabcd5b5c1"
+  apiKey: "some-api-key",
+  authDomain: "some-api-key.firebaseapp.com",
+  databaseURL: "https://some-api-key.firebaseio.com",
+  projectId: "some-api-key",
+  storageBucket: "some-api-key.appspot.com",
+  messagingSenderId: "some-api-key",
+  appId: "some-api-key:some-api-key:web:some-api-key"
 };
 
 // Initialize Firebase
